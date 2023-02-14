389823d3330b180609fd37e46bb7d96bfb3a7026
Felipe <feferreira.sf@gmail.com>
2023-02-14 00:49:38 -0300

    feat: context to get select values from navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
+import { createContext, useState } from "react"
+import { Navbar } from "./components/Navbar"
 import { ScrollToTop } from "./components/ScrollToTop"
 import { Home } from "./pages/Home"
 import { GlobalStyle } from "./theme/Global"
 import { Theme } from "./theme/Theme"
 
+export const SelectHouseAndAncestryContext = createContext({
+  selectedHouse: 'all',
+  selectedAncestry: 'all'
+})
+
+const SelectHouseAndAncestryProvider = ({ selectedHouse, selectedAncestry, children }) => {
+  return (
+    <SelectHouseAndAncestryContext.Provider value={{ selectedHouse, selectedAncestry }}>
+      {children}
+    </SelectHouseAndAncestryContext.Provider>
+  )
+}
+
 function App() {
+  const [selectedHouse, setSelectedHouse] = useState('all')
+  const [selectedAncestry, setSelectedAncestry] = useState('all')
+
   return (
     <Theme>
       <GlobalStyle />
       <ScrollToTop />
-      <Home />
+      <SelectHouseAndAncestryProvider selectedHouse={selectedHouse} selectedAncestry={selectedAncestry}>
+        <Navbar setSelectedHouse={setSelectedHouse} setSelectedAncestry={setSelectedAncestry} selectedHouse={selectedHouse} selectedAncestry={selectedAncestry} />
+        <Home />
+      </SelectHouseAndAncestryProvider>
     </Theme>
   )
 }
