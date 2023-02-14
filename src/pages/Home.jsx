1f8d8d6b5a2486aff307de5178214d5c0d95ed08
Felipe <feferreira.sf@gmail.com>
2023-02-14 00:51:27 -0300

    feat: select house and ancestry context to filter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import styled from "styled-components"
+import { SelectHouseAndAncestryContext } from "../App"
 import { CharacterCard } from "../components/CharacterCard"
 import { Loading } from "../components/Loading/Loading"
 import { useFecthCharacters } from "../hooks/useFetchCharacters"
@@ -22,23 +23,43 @@ export const Home = () => {
     )
   }
 
+  const { selectedAncestry, selectedHouse } = useContext(SelectHouseAndAncestryContext)
+
+  console.log('Home ->', selectedHouse)
+  // console.log('Home ->', selectedAncestry)
+
   return (
-    <CharactersList>
-      {characters.slice(0, charactersToShow).map(character => (
-        <CharacterCard character={character} />
-      ))}
+    <HomeContainer>
+
+      <CharactersList>
+        {characters
+          .filter(character => selectedHouse === 'all' || character.house === selectedHouse)
+          .filter(character => selectedAncestry === 'all' || character.ancestry === selectedAncestry)
+          .slice(0, charactersToShow).map(character => (
+            <CharacterCard key={character.id} character={character} />
+          ))}
+      </CharactersList>
       <button onClick={handleShowMore}>Exibir Mais</button>
-    </CharactersList>
+    </HomeContainer>
   )
 }
 
-const CharactersList = styled.main`
-  max-width: 1750px;
+const HomeContainer = styled.main`
+  max-width: 1740px;
   width: 100%;
 
   margin: auto;
 
-padding-bottom: 4px;
+  padding-bottom: 8px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const CharactersList = styled.div`
+  width: 100%;
 
   display: flex;
   flex-wrap: wrap;
